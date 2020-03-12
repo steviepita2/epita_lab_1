af56b4cdfc0d4d9f7367d12eb7f1e7eab3fc0fa6
steviepita2 <lavenustevie@gmail.com>
2020-03-12 15:43:45 +0000

    done

---
+++ fewd_lab_1/script.js
@@ -0,0 +1,28 @@
+// Question 4
+var firstName = "Stevie";
+var favouriteColor = "Purple";
+
+// Question 5
+var favouriteFood = "Raclette";
+var city = "Paris";
+
+//Question 8 & 9
+//Shows the variable in the console
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+
+//Question 10
+var phrase = " A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six ="indigo";
+var seven = "violet";
+
+//Question 11
+
+console.log(phrase+" "+one+", "+two+", "+three+", "+four+", "+five+", "+six+", "+seven+" "+endingPhrase);
